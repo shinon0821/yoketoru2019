5c54c3f7e6da49986534b4a67906a005a44c28fa
shinon0821 <49701829+shinon0821@users.noreply.github.com>
2019-06-28 15:46:56 +0900

    yoketoru2019

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    bool sceneChanged = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sceneChanged == true) return;
+
+        if(Input.GetMouseButtonDown(0) ||Input.GetButtonDown("Jump"))
+        {
+            sceneChanged = true;
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Game");
+        }
+    }
+}
